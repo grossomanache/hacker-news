f315ea03c3d530d27fe9a83b53202ef8e129128a
Elías Heresi <elias.heresi@gmail.com>
2022-08-28 00:43:15 +0200

    Added hover function to pagination numbers

--- src/component/Filter/FilterContainer.tsx
+++ src/component/Filter/FilterContainer.tsx
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 const FilterContainer = styled.div`
   select {
+    cursor: pointer;
     width: 240px;
     height: 32px;
-    margin-left: 30px;
+    margin: 23px 0 23px 30px;
     size: 20;
   }
 `;
--- src/component/Pagination/PaginationContainer.tsx
+++ src/component/Pagination/PaginationContainer.tsx
@@ -4,7 +4,9 @@ const PaginationContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
+  margin: 76px 0;
   p {
+    cursor: pointer;
     height: 32px;
     line-height: 32px;
     width: 32px;
@@ -12,6 +14,9 @@ const PaginationContainer = styled.div`
     border-radius: 6px;
     margin: 0 4px;
     text-align: center;
+    &:hover {
+      background-color: #b1b1b1;
+    }
     &.active {
       color: #ffffff;
       background-color: #1890ff;
