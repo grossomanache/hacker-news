e050a935ed282529d4147e908da6c490e8c1411d
Elías Heresi <elias.heresi@gmail.com>
2022-08-28 00:24:29 +0200

    Alligned pagination at the center of the page

--- src/component/Pagination/PaginationContainer.tsx
+++ src/component/Pagination/PaginationContainer.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const PaginationContainer = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: center;
   p {
     height: 32px;
     line-height: 32px;
