44641053241cd1f59f680b6b4c6f2a8f8b54181e
Elías Heresi <elias.heresi@gmail.com>
2022-08-27 17:56:49 +0200

    Created article list component, mapping each article from the api

--- src/component/ArticlePreviewList/ArticlePreviewList.tsx
+++ src/component/ArticlePreviewList/ArticlePreviewList.tsx
@@ -1,15 +1,28 @@
+import { ArticleCollection } from "../../interfaces/ArticlesInterfaces";
+import { useAppSelector } from "../../redux/store/hooks";
 import ArticlePreview from "../ArticlePreview/ArticlePreview";
 import ArticlePreviewListContainer from "./ArticlePreviewListContainer";
 
 const ArticlePreviewList = (): JSX.Element => {
+  const { collection }: ArticleCollection = useAppSelector(
+    (state) => state.articles
+  );
   return (
     <ArticlePreviewListContainer>
-      <ArticlePreview
-        author="Author"
-        time="3"
-        like={true}
-        title="React article"
-      />
+      <>
+        {collection.length > 0 &&
+          collection.map((article) => {
+            const { author, creationDate, title } = article;
+            return (
+              <ArticlePreview
+                author={author}
+                like={true}
+                time={"2"}
+                title={title}
+              />
+            );
+          })}
+      </>
     </ArticlePreviewListContainer>
   );
 };
