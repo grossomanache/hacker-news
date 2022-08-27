41e2a5d637356efa01f5234e5190f708596b1d0e
Elías Heresi <elias.heresi@gmail.com>
2022-08-27 23:59:16 +0200

    Pagination click is able to change the page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,17 @@ import { useEffect } from "react";
 import ArticlePreviewList from "../component/ArticlePreviewList/ArticlePreviewList";
 import Filter from "../component/Filter/Filter";
 import Pagination from "../component/Pagination/Pagination";
-import { useAppDispatch } from "../redux/store/hooks";
+import { PaginationState } from "../interfaces/PagesInterfaces";
+import { useAppDispatch, useAppSelector } from "../redux/store/hooks";
 import { loadArticlesThunk } from "../redux/thunks/articleThunks";
 import HomeContainer from "./HomeContainer";
 
 const Home = (): JSX.Element => {
   const dispatch = useAppDispatch();
+  const { page }: PaginationState = useAppSelector(({ page }) => page);
   useEffect(() => {
     const keyword = localStorage.getItem("filter") as string;
-    dispatch(loadArticlesThunk({ searchTerm: keyword }));
+    dispatch(loadArticlesThunk({ searchTerm: keyword, page }));
   });
   return (
     <HomeContainer>
