f460b2b4955332bd4d80cd6abe2e3232a917ce55
Elías Heresi <elias.heresi@gmail.com>
2022-08-28 17:32:58 +0200

    Added style for selected button

--- src/component/FavoritesButton/FavoritesButtonContainer.tsx
+++ src/component/FavoritesButton/FavoritesButtonContainer.tsx
@@ -4,13 +4,19 @@ const FavoritesButtonContainer = styled.div`
   display: flex;
   flex-direction: row;
   font-family: "Roboto", sans-serif;
+  font-weight: 500;
   color: #606060;
   p {
     border: solid 1px #d6d6d6;
+    border-radius: 2px;
     height: 31px;
     width: 98px;
     line-height: 28px;
     text-align: center;
+    &.active {
+      border-color: #1797ff;
+      color: #1797ff;
+    }
   }
 `;
 
