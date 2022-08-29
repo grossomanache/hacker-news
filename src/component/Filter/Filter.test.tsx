bc26d6e582b0dd012354894299ef9a6ba09319e3
Elías Heresi <elias.heresi@gmail.com>
2022-08-29 08:01:08 +0200

    Tested filter dispatching actions

--- src/component/Filter/Filter.test.tsx
+++ src/component/Filter/Filter.test.tsx
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
+import { changePageActionCreator } from "../../redux/features/pageSlice";
 import store from "../../redux/store/store";
 import Filter from "./Filter";
 
@@ -28,7 +29,7 @@ describe("Given the Filter Component", () => {
       expect(searchedOptions).toHaveLength(expectedNumberOfItems);
     });
   });
-  describe("When an option is clicked", () => {
+  describe("When the dropdown value is changed to 'React'", () => {
     test("Then actions will be dispatched twice", () => {
       render(
         <Provider store={store}>
@@ -43,6 +44,19 @@ describe("Given the Filter Component", () => {
       const expectedNumberOfCalls = 2;
 
       expect(mockDispatch).toHaveBeenCalledTimes(expectedNumberOfCalls);
+    });
+    test("Then the changePage action will have been called", () => {
+      render(
+        <Provider store={store}>
+          <Filter />
+        </Provider>
+      );
+
+      const searchedDropdown = screen.getByRole("combobox");
+
+      fireEvent.change(searchedDropdown, { target: { value: "React" } });
+
+      expect(mockDispatch).toHaveBeenCalledWith(changePageActionCreator(1));
     });
   });
 });
